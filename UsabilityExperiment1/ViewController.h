adc04c7917e5362d3c0dacf2b767fd2a7e77eaae
Sergey Lomov <sergeylomov@rostyslav.local>
2015-11-26 19:50:46 +0200

    Initial Commit

---
+++ UsabilityExperiment1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UsabilityExperiment1
+//
+//  Created by Sergey Lomov on 11/26/15.
+//  Copyright © 2015 Sergey Lomov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UsabilityExperiment1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UsabilityExperiment1
+//
+//  Created by Sergey Lomov on 11/26/15.
+//  Copyright © 2015 Sergey Lomov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
